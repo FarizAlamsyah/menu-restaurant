b9fcd57b140326d94b5a4dc9da3d9e905c9b973d
FarizAlamsyah <farizalamsyah22@gmail.com>
2020-04-28 21:23:38 +0700

    hellow

--- index.js
+++ index.js
@@ -50,5 +50,11 @@ function sortAlphabet(a, b){
     a = a.toLowerCase();
     b = b.toLowerCase();
 
-    return (a < b) ? -1 : (a > b) ? 1 : 0;
+    if (a < b) {
+    	return -1;
+    }else if (a > b) {
+    	return 1;
+    }else{
+    	return 0;
+    }
 }
